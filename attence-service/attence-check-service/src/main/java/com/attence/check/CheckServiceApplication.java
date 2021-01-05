bd1580c119ce839b7bca65785dd9679dc8d89122
softthink-csp <bing527309700@sina.com>
2021-01-05 10:00:21 +0800

    添加consule注册中心

--- attence-service/attence-check-service/src/main/java/com/attence/check/CheckServiceApplication.java
+++ attence-service/attence-check-service/src/main/java/com/attence/check/CheckServiceApplication.java
@@ -2,8 +2,10 @@ package com.attence.check;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CheckServiceApplication {
 
     public static void main(String[] args) {
